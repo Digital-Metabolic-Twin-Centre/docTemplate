fa1187400e7229948df861182871b19b8945dfa2
farid <farid.zare.edu@gmail.com>
2026-04-21 13:51:49 +0100

    Clean generated artifacts and update copyright owner

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 project = "Student Project Documentation"
-author = "Student"
+author = "Digital Metabolic Twin Centre"
 copyright = f"{datetime.now().year}, {author}"
 
 extensions = [
